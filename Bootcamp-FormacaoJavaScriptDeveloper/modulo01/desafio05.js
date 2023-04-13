95cec4bd11da436dd541919c78528cb1de171b65
Gabriel Bonifacio Possomato <gabrielb.possomato@gmail.com>
2023-04-13 10:14:23 -0300

    att desafio05

--- Bootcamp-FormacaoJavaScriptDeveloper/modulo01/desafio05.js
+++ Bootcamp-FormacaoJavaScriptDeveloper/modulo01/desafio05.js
@@ -3,25 +3,24 @@ Elabore um algoritmo que calcule o que deve ser pago por um produto, considerand
 Utilize os códigos da tabela a seguir para ler qual a condição de pagamento escolhida e efetuar o cálculo adequado
 
 Código condição de pagamento:
-- crédito à vista, 10% de desconto
-- dinheiro ou pix, 15% de desconto
-- em 2x, preço normal
-- acima de 2x, + 10% de juros
+1 - crédito à vista, 10% de desconto
+2 - dinheiro ou pix, 15% de desconto
+3 - em 2x, preço normal
+4 - acima de 2x, + 10% de juros
 */
 
 const preco = 900
-const metodo = 'crédito'
-const parcelas = 4
+const metodo = 2
 
-if (metodo == 'pix' || metodo == 'dinheiro'){
-    novoPreco = preco - (preco * 0.15)
-    console.log(`O valor a ser pago será R$${novoPreco}`)
-} else if(metodo == 'crédito' && parcelas == 1){
+if (metodo == 1){
     novoPreco = preco - (preco * 0.10)
-    console.log(`O valor a ser pago será R$${novoPreco}`)
-} else if(metodo == 'crédito' && parcelas == 2){
-    console.log(`O valor a ser pago será R$${preco}`)
-} else if(metodo == 'crédito' && parcelas > 2){
+    console.log(`o valor a ser pago será R$${novoPreco}`)
+} else if (metodo == 2){
+    novoPreco = preco - (preco * 0.15)
+    console.log(`o valor a ser pago será R$${novoPreco}`)
+} else if (metodo == 3){
+    console.log(`o valor a ser pago será R$${preco}`)
+} else if (metodo == 4){
     novoPreco = preco + (preco * 0.10)
-    console.log(`O valor a ser pago será R$${novoPreco}`)
+    console.log(`o valor a ser pago será R$${novoPreco}`)
 }
