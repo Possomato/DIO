02f3a7a3e8a9ae4c1f9df2fe1444bf8983cfce2e
Gabriel Bonifacio Possomato <gabrielb.possomato@gmail.com>
2023-04-19 21:14:15 -0300

    organizing

--- Bootcamp-FormacaoJavaScriptDeveloper/modulo02/POKEDEX/assets/js/main.js
+++ Bootcamp-FormacaoJavaScriptDeveloper/modulo02/POKEDEX/assets/js/main.js
@@ -1,6 +1,5 @@
 const offset = 0
 const limit = 10
-
 const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
 
 function convertPokemonToLi(pokemon){
@@ -19,14 +18,9 @@ function convertPokemonToLi(pokemon){
     `
 }
 
-
-fetch(url)
-    .then((response) => response.json())
-    .then((jsonBody) => jsonBody.results)
-    .then((pokemons) => {
-        for (let i = 0; i < pokemons.length; i++) {
-            const pokemon = pokemons[i];
-            pokemonList.innerHTML += convertPokemonToLi(pokemon)
-        }
-    })
-    .catch.catch((error) => console.errer(error))
+pokeApi.getPokemons().then((pokemons) => {
+    for (let i = 0; i < pokemons.length; i++) {
+        const pokemon = pokemons[i];
+        pokemonList.innerHTML += convertPokemonToLi(pokemon)
+    }
+})
