cd16d1218ac1cf443dc6f5070262b72836e244f7
Gabriel Possomato <gabrielb.possomato@gmail.com>
2023-07-03 14:49:52 -0300

    att

--- javascript/Bootcamp-FormacaoJavaScriptDeveloper/modulo04/portifolio/assets/js/main.js
+++ javascript/Bootcamp-FormacaoJavaScriptDeveloper/modulo04/portifolio/assets/js/main.js
@@ -1,4 +1,32 @@
+
+
+
+function updateProfileIndo(profileData){
+  const photo = document.getElementById('profile.photo')
+  photo.src = profileData.photo
+  photo.alt = profileData.name
+
+  const name = document.getElementById('profile.name')
+  name.innerText = profileData.name
+
+  const job = document.getElementById('profile.job')
+  job.innerText = profileData.job
+
+  const location = document.getElementById('profile.location')
+  location.innerText = profileData.location
+
+  const phone = document.getElementById('profile.phone')
+  phone.innerText = profileData.phone
+  phone.href = `https://api.whatsapp.com/send?phone=${profileData.phone}`
+
+  const mail = document.getElementById('profile.mail')
+  mail.innerText = profileData.mail
+  mail.href = `mailto:${profileData.mail}`
+
+  
+}
+
 (async () => {
   const profileData = await fetchProfileData()
-  console.log(profileData)
+  updateProfileIndo(profileData)
 })()
